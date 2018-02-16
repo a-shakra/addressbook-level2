f41a011a40dacbb69bf09637541a8864791fb09d
a.shakra <shakra.anas@gmail.com>
2018-02-16 17:30:50 +0800

    Addressbook List command now orders entry by name

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -1,7 +1,11 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -20,6 +24,20 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        List<ReadOnlyPerson> listOfSortedPersons = new ArrayList<>();
+        for (int i = 0; i < allPersons.size(); i++) { // Copy List from unsorted array to array that will be sorted
+            listOfSortedPersons.add(allPersons.get(i));
+        }
+        
+        for (int i = 0; i < listOfSortedPersons.size()-1; i++) // Two for loops will determine the correct position to place an entry in
+            for (int j = 0; j < listOfSortedPersons.size()-i-1; j++)
+                if (listOfSortedPersons.get(j).getName().toString().toLowerCase().compareTo(listOfSortedPersons.get(j+1).getName()
+                        .toString().toLowerCase()) > 0) // the names are converted to string and to lowercase to take advantage of the compareTo function
+                {
+                    ReadOnlyPerson temporary = listOfSortedPersons.get(j);
+                    listOfSortedPersons.set(j,listOfSortedPersons.get(j+1));
+                    listOfSortedPersons.set(j+1,temporary);
+                }
+        return new CommandResult(getMessageForPersonListShownSummary(listOfSortedPersons), listOfSortedPersons);
     }
 }
